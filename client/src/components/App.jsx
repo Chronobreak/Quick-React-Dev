71677bc176a4ce6f3e67b785828d8d446d56c2db
DDK <ddkgeorgetown@gmail.com>
2019-02-14 23:30:40 -0500

    init

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+    <div>
+      Rendering App.jsx! - from client/src/components/App.jsx
+    </div>
+    )
+  }
+}
+
+export default App;
